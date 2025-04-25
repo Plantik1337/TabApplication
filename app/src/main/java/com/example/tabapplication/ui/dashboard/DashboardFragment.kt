a60187001ec738bd03aed8a601450dab047f0b0e
Tatiana Soloveva (android plus, cohort 24) <tatjana.solov2014@yandex.ru>
2025-04-25 20:42:14 +0300

    Add DashBoard's tablayout with 3 tabs

--- app/src/main/java/com/example/tabapplication/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/tabapplication/ui/dashboard/DashboardFragment.kt
@@ -7,11 +7,17 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.tabapplication.R
 import com.example.tabapplication.databinding.FragmentDashboardBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class DashboardFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
+
+    private lateinit var tabLayoutMediator: TabLayoutMediator
+
+    //private val viewModel: DashboardViewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -28,15 +34,30 @@ class DashboardFragment : Fragment() {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.viewPagerDashboard.adapter =
+            DashboardViewPagerAdapter(requireActivity().supportFragmentManager, lifecycle)
+
+        val activity = requireActivity()
+
+        tabLayoutMediator = TabLayoutMediator(binding.tabDashboard, binding.viewPagerDashboard) { tab, position ->
+            when (position){
+                0 -> tab.text = activity.getString(R.string.first_dashboard_tab_title)
+                1 -> tab.text = activity.getString(R.string.second_dashboard_tab_title)
+                2 -> tab.text = activity.getString(R.string.third_dashboard_tab_title)
+            }
+        }
+        tabLayoutMediator.attach()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        tabLayoutMediator.detach()
     }
 }
---
+++ app/src/main/java/com/example/tabapplication/ui/dashboard/subFragments/FirstDashboardFragment.kt
@@ -0,0 +1,11 @@
+package com.example.tabapplication.ui.dashboard.subFragments
+
+import androidx.fragment.app.Fragment
+import com.example.tabapplication.R
+
+class FirstDashboardFragment() : Fragment(R.layout.fragment_first_dashboard) {
+
+    companion object{
+        fun newInstance() = FirstDashboardFragment()
+    }
+}
